5374cc02184a72fa04677708326c3b9fc8a6f66c
Ricky Rhonaldo Ortega Vital <78801704+RickyOrtega@users.noreply.github.com>
2023-07-30 16:07:16 -0500

    Quinta-entrega-tarea6

--- webapp-carrito-compras/src/main/java/org/rortega/apiservlet/webapp/carrito/models/Carro.java
+++ webapp-carrito-compras/src/main/java/org/rortega/apiservlet/webapp/carrito/models/Carro.java
@@ -5,32 +5,54 @@ import java.util.List;
 import java.util.Optional;
 
 public class Carro {
-    List<ItemCarro> itemsCarro;
+    private List<ItemCarro> items;
 
     public Carro() {
-        this.itemsCarro = new ArrayList<>();
+        this.items = new ArrayList<>();
     }
 
-    public void addItemCarro(ItemCarro itemCarro){
-
-        if(itemsCarro.contains(itemCarro)){
-            Optional<ItemCarro> optionalItemCarro = itemsCarro.stream()
+    public void addItemCarro(ItemCarro itemCarro) {
+        if (items.contains(itemCarro)) {
+            Optional<ItemCarro> optionalItemCarro = items.stream()
                     .filter(i -> i.equals(itemCarro))
                     .findAny();
-            if (optionalItemCarro.isPresent()){
+            if (optionalItemCarro.isPresent()) {
                 ItemCarro i = optionalItemCarro.get();
                 i.setCantidad(i.getCantidad()+1);
             }
         } else {
-            this.itemsCarro.add(itemCarro);
+            this.items.add(itemCarro);
+        }
+    }
+    public List<ItemCarro> getItemsCarro() {
+        return items;
+    }
+
+    public int getTotal() {
+        return items.stream().mapToInt(ItemCarro::getImporte).sum();
+    }
+
+    public void removeProductos(List<String> productoIds) {
+        if (productoIds != null) {
+            productoIds.forEach(this::removeProducto);
+            // que es lo mismo a:
+            // productoIds.forEach(productoId -> removeProducto(productoId));
         }
     }
 
-    public List<ItemCarro> getItemsCarro() {
-        return itemsCarro;
+    public void removeProducto(String productoId) {
+        Optional<ItemCarro> producto = findProducto(productoId);
+        producto.ifPresent(itemCarro -> items.remove(itemCarro));
     }
 
-    public Integer getTotal(){
-        return itemsCarro.stream().mapToInt(ItemCarro::getImporte).sum();
+    public void updateCantidad(String productoId, int cantidad) {
+        Optional<ItemCarro> producto = findProducto(productoId);
+        producto.ifPresent(itemCarro -> itemCarro.setCantidad(cantidad));
+    }
+
+    private Optional<ItemCarro> findProducto(String productoId) {
+        return  items.stream()
+                .filter(itemCarro -> productoId.equals(Long.toString(itemCarro.getProducto().getId())))
+                .findAny();
     }
 }
--- webapp-carrito-compras/src/main/webapp/carro.jsp
+++ webapp-carrito-compras/src/main/webapp/carro.jsp
@@ -18,6 +18,7 @@ import="org.rortega.apiservlet.webapp.carrito.models.*"
         <% if(carro == null || carro.getItemsCarro().isEmpty()){%>
             <p>¡Lo sentimos, no hay productos en el carro de compras!</p>
         <%} else { %>
+        <form name="formcarro" action="<%=request.getContextPath().concat("/actualizar-carro")%>" method="post">
             <table>
                 <tr>
                     <th>Id</th>
@@ -26,6 +27,7 @@ import="org.rortega.apiservlet.webapp.carrito.models.*"
                     <th>Precio</th>
                     <th>Cantidad</th>
                     <th>Total</th>
+                    <th>Eliminar</th>
                 </tr>
                 <%for(ItemCarro item: carro.getItemsCarro()){%>
                 <tr>
@@ -33,14 +35,17 @@ import="org.rortega.apiservlet.webapp.carrito.models.*"
                     <td><%=item.getProducto().getTipo()%></td>
                     <td><%=item.getProducto().getNombre()%></td>
                     <td><%=item.getProducto().getPrecio()%></td>
-                    <td><%=item.getCantidad()%></td>
+                    <td><input type="text" size="4" name="cant_<%=item.getProducto().getId()%>" value="<%=item.getCantidad()%>" /></td>
                     <td><%=item.getImporte()%></td>
+                    <td><input type="checkbox" value="<%=item.getProducto().getId()%>" name="deleteProductos" /></td>
                 </tr>
                 <%}%>
                 <tr>
                     <td colspan="4" style="text-align: right">Total: <span ><%=carro.getTotal()%></span></td>
                 </tr>
             </table>
+        <a href="javascript:document.formcarro.submit();">Actualizar</a>
+    </form>
         <%}%>
         <p><a href="<%=request.getContextPath().concat("/productos")%>">Seguir comprando</a></p>
         <p><a href="<%=request.getContextPath().concat("/index.html")%>">Inicio</a></p>
