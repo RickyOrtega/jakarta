f53508bd0196a3f9030b2b39967c02d56e04dfab
Ricky Rhonaldo Ortega Vital <78801704+RickyOrtega@users.noreply.github.com>
2023-07-31 04:44:30 -0500

    Delete webapp-carrito-compras-listeners directory

--- webapp-carrito-compras-listeners/src/main/java/org/rortega/apiservlet/webapp/carrito/services/LoginService.java
+++
@@ -1,9 +0,0 @@
-package org.rortega.apiservlet.webapp.carrito.services;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-import java.util.Optional;
-
-public interface LoginService {
-    Optional<String> getUsername(HttpServletRequest req);
-}
